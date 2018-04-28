90d40c89ab5314c7101fe25673e2b448cb296451
Roman <aventawonk@gmail.com>
2018-04-28 23:57:17 +0300

    Move to Log4j

--- src/main/java/com/greatCouturierGame/Bot.java
+++ src/main/java/com/greatCouturierGame/Bot.java
@@ -1,18 +1,20 @@
 package com.greatCouturierGame;
 
+import org.apache.logging.log4j.Logger;
+
 import java.util.Date;
 
 public class Bot implements Runnable {
 
     private String uid;
     private String authToken;
+    private static Logger logger = Main.getLogger();
 
     public Bot(String uid, String authToken) {
         this.uid = uid;
         this.authToken = authToken;
     }
 
-    @Override
     public void run() {
 
         while (true) {
@@ -24,22 +26,31 @@ public class Bot implements Runnable {
             final long[] sat = gamer.getSkillsAvTime();
             final long currentTime = new Date().getTime();
 
+            logger.info("New cycle started!");
             for (int i = 0; i < 5; i++) {
+                // Check is new tech available now?
+                if (gamer.isNewTechAvailable()) {
+                    String[] availableTechIds = gamer.getAvailableTechIds();
+                    gamer.researchTech(availableTechIds[0]);
+                    logger.info("New tech was researched");
+                }
+
                 final long skillAvTime = sat[i];
                 if (skillAvTime >= currentTime) {
                     final int skillCooldown = (int) Math.ceil((skillAvTime - currentTime) / 1000);
-                    System.out.println("Skill №" +(i+1)+ " is not available now! Next in "+ Bot.getFormattedTime(skillCooldown));
+                    logger.info("Skill №" +(i+1)+ " is not available now! Next in "+ Bot.getFormattedTime(skillCooldown));
                     continue;
                 }
 
                 try {
                     final int taskTimeInc = gamer.doTask(i);
                     sat[i] = new Date().getTime() + taskTimeInc + 3000;
-                    System.out.println("Skill №" + (i+1) + " successfully applied!");
+                    logger.info("Skill №" + (i+1) + " successfully applied!");
 
-                    Thread.sleep(5000);
+                    // Simulate gamers reaction time
+                    Thread.sleep(2400);
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    logger.error(e);
                 }
 
             }
@@ -53,13 +64,13 @@ public class Bot implements Runnable {
             }
 
             long soonSkillAvTime = sat[soonSkillNumber] - new Date().getTime();
-            System.out.println("Skill №"+ (soonSkillNumber+1) +". Waiting "+ Bot.getFormattedTime(soonSkillAvTime/1000));
+            logger.info("Skill №"+ (soonSkillNumber+1) +". Waiting "+ Bot.getFormattedTime(soonSkillAvTime/1000) + "\n");
             gamer.closeGame();
 
             try {
                 Thread.sleep(soonSkillAvTime + 10000);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                logger.error(e);
             }
         }
     }
