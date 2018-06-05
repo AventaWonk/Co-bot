89021a8a06a1fd2b9f02900c85b45f90d37a5c2a
Roman <aventawonk@gmail.com>
2018-06-05 23:57:34 +0300

    Update main method

--- src/main/java/com/greatCouturierGame/Main.java
+++ src/main/java/com/greatCouturierGame/Main.java
@@ -3,29 +3,40 @@ package com.greatCouturierGame;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiConsumer;
 
 public class Main {
     public final static Logger logger = LogManager.getLogger("FileLogger");
+    private static final String accountsFile = "accounts.txt";
 
     public static void main(String[] args) {
         Map<String, String> accountsList = new HashMap<>();
-//        accountsList.put("", "");
-//        accountsList.forEach();
-//        try (FileReader fr = new FileReader("accounts.txt")) {
-//            while (fr.) {
-//
-//            }
-//        } catch (FileNotFoundException e) {
-//            logger.error();
-//        } catch (IOException e) {
-//            logger.fatal();
-//        }
+        try (BufferedReader bfr = new BufferedReader(new FileReader(Main.accountsFile))) {
+            String accountString;
+            while ((accountString = bfr.readLine()) != null) {
+                String[] accountParts = accountString.split(":");
+                accountsList.put(accountParts[0], accountParts[1]);
+            }
+        } catch (FileNotFoundException e) {
+            Main.logger.fatal("File"+ Main.accountsFile +"not found");
+        } catch (IOException e) {
+            Main.logger.fatal("");
+        } catch (NullPointerException e) {
+            Main.logger.fatal("Bad accounts structure. Check!");
+        }
 
-
-        Thread botThread = new Thread(bot);
-        botThread.start();
+        BiConsumer<String, String> engine = (uid, authToken) -> {
+            Bot bot = new Bot(uid, authToken);
+            Thread botThread = new Thread(bot);
+            botThread.start();
+        };
+        accountsList.forEach(engine);
     }
 
 }
