b17ee3a3444d1e1e1bba89019c4f86cee4d0465e
Roman <aventawonk@gmail.com>
2018-07-14 23:43:13 +0300

    Add test for getTypeOfQuery method

--- src/test/java/com/greatCouturierGame/connection/QueryParserTest.java
+++ src/test/java/com/greatCouturierGame/connection/QueryParserTest.java
@@ -6,32 +6,34 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class QueryParserTest {
 
-    private static List<String> testResponseList;
-
-    static {
-        testResponseList = Arrays.asList(
+    private static List<String> testResponseList = Arrays.asList(
                 "Type:SyncTimeResponse;ServerTime:1520639998765;",
-                "Type:ConnectResponse;Name:Test;EyeColor:1;HairColor:2;SwimColor:3;NoseType:4;"
-        );
-    }
+                "Type:ConnectResponse;Name:Test;EyeColor:1;HairColor:2;SwimColor:3;NoseType:;"
+    );
+
 
     @Test
     void getParameter() {
         QueryParser qp = new QueryParser(testResponseList.get(0));
-        assertDoesNotThrow(() -> {
-            assertEquals("1520639998765", qp.getParameter("ServerTime"));
-        });
 
-        assertThrows(NotContainsParameterException.class, () -> qp.getParameter("Test"));
+        assertEquals("1520639998765", qp.getParameter("ServerTime"));
+        assertEquals("Test", qp.getParameter("Name"));
+        assertEquals("", qp.getParameter("NoseType"));
+        assertNull(qp.getParameter("Test"));
     }
 
     @Test
     void getTypeOfQuery() {
+        String testTypeOfQuery1 = QueryParser.getTypeOfQuery(testResponseList.get(0));
+        String testTypeOfQuery2 = QueryParser.getTypeOfQuery(testResponseList.get(2));
 
+        assertEquals("SyncTimeResponse", testTypeOfQuery1);
+        assertEquals("ConnectResponse", testTypeOfQuery2);
     }
 
     @Test
@@ -43,14 +45,18 @@ class QueryParserTest {
         );
 
         assertEquals("1520631234567", parameters.get("ServerTime"));
-        assertEquals(null, parameters.get("Server"));
+        assertNull(parameters.get("Test"));
+        assertNull(parameters.get("Test2"));
 
-        parameters = qp.setQuery(testResponseList.get(1)).getParameters(
-                "Name",
-                "SwimColor"
-        );
+        parameters = qp.setQuery(testResponseList.get(1))
+                .getParameters(
+                        "Name",
+                        "SwimColor",
+                        "NoseType"
+                );
 
         assertEquals("Test", parameters.get("Name"));
         assertEquals("3", parameters.get("SwimColor"));
+        assertEquals(3, parameters.size());
     }
 }
