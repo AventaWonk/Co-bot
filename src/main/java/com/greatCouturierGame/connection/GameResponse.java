a09625879357bf0a70a0dffb3e24c125117da877
Roman <aventawonk@gmail.com>
2018-07-20 23:42:15 +0300

    Update constructor of game response class

--- src/main/java/com/greatCouturierGame/connection/GameResponse.java
+++ src/main/java/com/greatCouturierGame/connection/GameResponse.java
@@ -1,15 +1,28 @@
 package com.greatCouturierGame.connection;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class GameResponse {
 
     private Map<String, String> response;
 
-    public GameResponse(Map<String, String> response) {
+    public GameResponse(byte[] responseData) {
+        Map<String, String> response = new HashMap<>();
+        int lastEOL = -1;
+        for (int i = 0; i < responseData.length; i++) {
+            if (responseData[i] == 0) {
+                String subResponse = new String(responseData, lastEOL + 1,  i - lastEOL);
+                String type = QueryParser.getType(subResponse);
+                response.put(type, subResponse);
+                lastEOL = i;
+            }
+        }
+
         this.response = response;
     }
+
 
     public GameResponse shouldContain(String queryType) throws IOException {
         if (!this.response.containsKey(queryType)) {
