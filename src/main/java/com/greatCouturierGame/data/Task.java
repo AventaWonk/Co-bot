cb25e9c9178d84afa7f5b3baf39e878bb359dd88
Roman <aventawonk@gmail.com>
2018-07-31 23:46:39 +0300

    Move Task class to data package

---
+++ src/main/java/com/greatCouturierGame/data/Task.java
@@ -0,0 +1,30 @@
+package com.greatCouturierGame.data;
+
+public final class Task {
+
+    private String id;
+    private long cooldown;
+
+    private Task(String id, long cooldown) {
+        this.id = id;
+        this.cooldown = cooldown;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public long getCooldown() {
+        return cooldown;
+    }
+
+    public static Task[] getAllTasks() {
+        return new Task[] {
+                new Task("1", 602000),
+                new Task("3", 3602000),
+                new Task("6", 14401000),
+                new Task("15", 28801000)
+        };
+    }
+
+}
