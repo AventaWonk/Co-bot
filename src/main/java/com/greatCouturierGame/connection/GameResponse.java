945c7e84a39eaa9cfab5547d7c8487a410be88d6
Roman <aventawonk@gmail.com>
2018-08-25 23:56:54 +0300

    Update game response class

--- src/main/java/com/greatCouturierGame/connection/GameResponse.java
+++ src/main/java/com/greatCouturierGame/connection/GameResponse.java
@@ -1,31 +1,34 @@
 package com.greatCouturierGame.connection;
 
+import com.greatCouturierGame.adapter.IOSocketService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class GameResponse {
 
-    private Map<String, String> response;
+    private static final Logger logger = LogManager.getLogger(GameResponse.class);
+
+    private Map<String, String> responseMap;
+    
+    public GameResponse() {
+        this.responseMap = new HashMap<>();
+    }
 
     public GameResponse(byte[] responseData) {
-        Map<String, String> response = new HashMap<>();
-        int lastEOL = -1;
-        for (int i = 0; i < responseData.length; i++) {
-            if (responseData[i] == 0) {
-                String subResponse = new String(responseData, lastEOL + 1,  i - lastEOL);
-                String type = QueryParser.getType(subResponse);
-                response.put(type, subResponse);
-                lastEOL = i;
-            }
-        }
+        this.responseMap = GameResponse.parseResponse(responseData);
+    }
 
-        this.response = response;
+    public void addPart(byte[] responseData) {
+        this.responseMap.putAll(GameResponse.parseResponse(responseData));
     }
 
 
     public GameResponse shouldContain(String queryType) throws IOException {
-        if (!this.response.containsKey(queryType)) {
+        if (!this.responseMap.containsKey(queryType)) {
             throw new IOException("Response does not contain query "+ queryType);
         }
 
@@ -33,26 +36,52 @@ public class GameResponse {
     }
 
     public GameResponse shouldNotContain(String queryType) throws IOException {
-        if (this.response.containsKey(queryType)) {
+        if (this.responseMap.containsKey(queryType)) {
             throw new IOException("Response contains query "+ queryType);
         }
 
         return this;
     }
 
-    public Map<String, String> getResponse() {
-        return response;
-    }
-
-    public QueryParserImpl getQuery(String queryType) throws IOException  {
-        if (queryType.isEmpty() || !response.containsKey(queryType)) {
-            throw new IOException("Response does not contain query "+ queryType);
+    public QueryParserImpl getQuery(String queryType) {
+        if (queryType.isEmpty() || !responseMap.containsKey(queryType)) {
+            return null;
         }
 
-        return new QueryParserImpl(response.get(queryType));
+        return new QueryParserImpl(responseMap.get(queryType));
     }
 
     public boolean isContains(String queryType) {
-        return this.response.containsKey(queryType);
+        return this.responseMap.containsKey(queryType);
     }
+
+    public Map<String, String> getResponseMap() {
+        return responseMap;
+    }
+
+    protected static Map<String, String> parseResponse(byte[] responseData) {
+        logger.info("Response parsing started");
+        Map<String, String> response = new HashMap<>();
+        int lastDelimiter = -1;
+        for (int i = 0; i < responseData.length; i++) {
+            if (responseData[i] == IOSocketService.EOL) {
+                String subResponse = new String(responseData, lastDelimiter + 1,  i - lastDelimiter);
+                String type = QueryParser.getType(subResponse);
+                if (type == null) {
+                    logger.error("Type was not found\n"+
+                            "Response: "+ subResponse);
+                    continue;
+                }
+
+                response.put(type, subResponse.substring(type.length() + 6, subResponse.length() - 1));
+                lastDelimiter = i;
+                logger.info("Type "+ type +" parsed");
+            }
+        }
+
+        logger.info("Parsing successfully completed");
+
+        return response;
+    }
+
 }
