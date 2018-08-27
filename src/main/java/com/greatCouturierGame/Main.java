b95cf5e7c3bb3e585ae46f45695d0a8c5124f1bf
Roman <aventawonk@gmail.com>
2018-08-27 22:57:56 +0300

    Update main class

--- src/main/java/com/greatCouturierGame/Main.java
+++ src/main/java/com/greatCouturierGame/Main.java
@@ -1,103 +1,56 @@
 package com.greatCouturierGame;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.greatCouturierGame.adapter.*;
+import com.greatCouturierGame.connection.GameAPI;
+import com.greatCouturierGame.connection.SocketGameAPI;
+import com.greatCouturierGame.data.AppSettings;
+import com.greatCouturierGame.logic.AbstractBotStrategy;
+import com.greatCouturierGame.logic.Bot;
+import com.greatCouturierGame.logic.SimpleBotStrategy;
+import com.greatCouturierGame.provider.AccountsProvider;
+import com.greatCouturierGame.provider.FileAccountsProvider;
+import com.greatCouturierGame.provider.JsonSettingsProvider;
+import com.greatCouturierGame.provider.SettingsProvider;
+import com.greatCouturierGame.validation.AccountsValidator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.*;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
 
 public class Main {
-    public static final Logger logger = LogManager.getLogger("FileLogger");
-    protected static final String SETTINGS_FILE = "settings.json";
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        File settingsFile = new File(Main.SETTINGS_FILE);
-        Settings settings;
-        try {
-            if (!settingsFile.exists()) {
-                Main.createSettingsFile();
-            }
+        SettingsProvider settingsProvider = new JsonSettingsProvider("settings.json");
+        AppSettings settings = settingsProvider.getSettings();
 
-            ObjectMapper mapper = new ObjectMapper();
-            settings = mapper.readValue(settingsFile, Settings.class);
-        } catch (IOException e) {
-            Main.logger.error("Bad settings file");
-            settings = Settings.getDefault();
+        AccountsProvider accountsProvider = new FileAccountsProvider(
+                settings.getAccountsFile(),
+                settings.getAccountPartsSeparator()
+        );
+        Map<String, String> accountsMap = accountsProvider.getAccounts();
+        if (settings.isAccountValidation()) {
+            accountsMap = AccountsValidator.getValid(accountsMap);
         }
 
-        final String accountsFileName = settings.getAccountsFile();
-        Map<String, String> accountsList = new HashMap<>();
-        try {
-            try (BufferedReader br = new BufferedReader(new FileReader(accountsFileName))) {
-                Main.logger.info("Accounts file found successfully");
-                int currentLine = 1;
-                String accountString;
-                while ((accountString = br.readLine()) != null) {
-                    String[] accountParts = accountString.split(settings.getAccountPartsSeparator());
-                    if (settings.isAccountValidation()) {
-                        boolean userIsValid = Main.isUserIdValid(accountParts[0]);
-                        boolean userAuthTokenIsValid = Main.isUserAuthTokenValid(accountParts[1]);
-                        if (userIsValid || userAuthTokenIsValid) {
-                            Main.logger.error("Error in accounts file"+ accountsFileName
-                                    +" at line: "+ currentLine
-                                    +", id is valid: "+ userIsValid
-                                    +", auth token is valid: " + userAuthTokenIsValid);
-                            continue;
-                        }
-                    }
-
-                    accountsList.put(accountParts[0], accountParts[1]);
-                    Main.logger.info("Account verified successfully at line: "+ currentLine);
-                    currentLine++;
-                }
-            }
-
-            if (accountsList.size() < 1) {
-                Main.logger.error("Accounts list is empty");
-                System.exit(1);
-            }
-
-            BiConsumer<String, String> engine = (uid, authToken) -> {
-                Bot bot = new Bot(uid, authToken);
-                Thread botThread = new Thread(bot);
-                botThread.start();
-            };
-
-            accountsList.forEach(engine);
-        } catch (FileNotFoundException e) {
-            Main.logger.fatal("File "+ accountsFileName +" not found!");
-            Main.createAccountsFile(accountsFileName);
-        } catch (IOException e) {
-            Main.logger.fatal(e);
+        if (accountsMap.size() < 1) {
+            Main.logger.error("Accounts list is empty");
+            System.exit(1);
         }
-    }
 
-    protected static boolean isUserIdValid(String id) {
-        return id.matches("^\\d{1,17}$");
-    }
+        BiConsumer<String, String> engine = (uid, authToken) -> {
+            RequestSigner requestSigner = new RequestSignerImpl();
+            SocketService socketService = new IOSocketService();
+            Communicator communicator = new SocketCommunicator(socketService, requestSigner);
+            GameAPI gameAPI = new SocketGameAPI(communicator);
 
-    protected static boolean isUserAuthTokenValid(String token) {
-        return token.matches("^\\w{32}$");
-    }
-
-    protected static void createAccountsFile(String fileName) {
-        File file = new File(fileName);
-        try {
-            file.createNewFile();
-            Main.logger.info("Accounts file "+ fileName +" created successfully");
-        } catch (IOException e) {
-            Main.logger.fatal("Can't create accounts file with name "+ fileName);
-        }
-    }
-
-    protected static void createSettingsFile() throws IOException {
-        new ObjectMapper()
-                .writeValue(new File(Main.SETTINGS_FILE), Settings.getDefault());
-//        Main.logger.fatal("Can't create settings file!");
-        Main.logger.info("Settings file created successfully");
+            AbstractBotStrategy botStrategy = new SimpleBotStrategy(gameAPI);
+            Bot bot = new Bot(botStrategy);
+            bot.run(uid, authToken);
+        };
+        accountsMap.forEach(engine);
     }
 
 }
